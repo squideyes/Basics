e4fb85c10116aa7ab1315e424b07c35b7bacebe0
Louis S. Berman <louis@squideyes.com>
2021-10-17 23:33:57 -0400

    Added IConfigurationExtendersTests class

---
+++ UnitTests/Extenders/IConfigurationExtendersTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using SquidEyes.Basics;
+using System.Collections.Generic;
+using Xunit;
+
+namespace SquidEyes.UnitTests
+{
+    public class IConfigurationExtendersTests
+    {
+        [Fact]
+        public void GetAsReturnsExpectedValues()
+        {
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new List<KeyValuePair<string, string>> 
+                {
+                    new KeyValuePair<string, string>("Number", 100.ToString())
+                })
+                .Build();
+
+            config.GetAs<int>("Number").Should().Be(100);
+            config.GetAs<string>("Number").Should().Be("100");
+        }
+    }
+}
