44e9639a45322fe6bc074cef204f6d948b412f93
Louis S. Berman <louis@squideyes.com>
2021-10-17 23:46:18 -0400

    Started to add tests to IEnumerableExtendersTests

---
+++ UnitTests/Extenders/IEnumerableExtendersTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using SquidEyes.Basics;
+using System.Collections.Generic;
+using Xunit;
+
+namespace SquidEyes.UnitTests.Extenders
+{
+    public class IEnumerableExtendersTests
+    {
+        [Fact]
+        public void ForEachIteratesAsExpected()
+        {
+            int index = 0;
+
+            ((IEnumerable<int>)new List<int> { 1, 2, 3 }).ForEach(
+                number =>
+                {
+                    index++;
+
+                    number.Should().Be(index);
+                });
+
+            index.Should().Be(3);
+        }
+    }
+}
